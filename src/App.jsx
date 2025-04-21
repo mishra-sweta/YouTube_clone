ce212bb4ec603edf88d996b1bdb48c5bfbde0b55
mishra-sweta <swetamishra.liluah@gmail.com>
2025-04-21 23:18:33 +0530

    skeleton components of the home page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Body from "./components/Body";
+
+function App() {
+  return (
+    <div className="text-3xl">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
